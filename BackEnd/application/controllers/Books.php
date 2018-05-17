8e16bb748044f1a91040e8962b533e1b74880beb
pio-kniaz <pio.kniaz@gmail.com>
2018-05-17 20:28:25 +0200

    Books_model creatend and connected with db

--- BackEnd/application/controllers/Books.php
+++ BackEnd/application/controllers/Books.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Books extends CI_Controller {
 
 	public function books_api()
+	
 	{
 		$this->load->model('Books_model');
 		$data=$this->Books_model->get_books();
