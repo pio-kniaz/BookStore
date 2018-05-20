a262624d935a92e773bdb01a9cc45cb0a3178d48
pio-kniaz <pio.kniaz@gmail.com>
2018-05-20 06:22:45 +0200

    Handle Submit fn added , test successed

--- BackEnd/application/controllers/Books.php
+++ BackEnd/application/controllers/Books.php
@@ -3,9 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Books extends CI_Controller {
 
+
 	public function books_api()
-	
 	{
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+		header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 		$this->load->model('Books_model');
 		$data=$this->Books_model->get_books();
 		$books['books']=$data;
@@ -14,5 +17,30 @@ class Books extends CI_Controller {
 		// $this->load->view("main",$books);
 		echo json_encode($books);
 	}
+
+	public function delete($id)
+	{
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+		header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+		$this->load->model('Books_model');
+		$this->Books_model->delete_book($id);
+	}
+
+	public function test()
+	{
+		$this->load->view('main');
+	}
+
+	public function create()
+	{
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+		header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+		$this->load->model('Books_model');
+		$this->Books_model->add_book();
+	}
+
+
 }
 ?><?php
